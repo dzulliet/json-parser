a693963eac96b3083cebae3c198e79a5cfa493f6
Julie Kopackova <julie.kopackova@gmail.com>
2021-01-26 13:03:10 +0100

    eslint, prettier, styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState } from 'react'
+import './App.css'
 
-function App() {
+const App = (): JSX.Element => {
+  const [json, setJson] = useState('')
+  const handleSetJson = useCallback((e) => setJson(e.target.value), [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="parser">
+        <div className="textArea">
+          <textarea value={json} onChange={handleSetJson} />
+        </div>
+        <div className="controls">controls here</div>
+        <div className="parsedData">parsed data here</div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
